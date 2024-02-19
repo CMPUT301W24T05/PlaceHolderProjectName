b72fbb10f8f66b06c101aca4a014f77f66360542
Max <mwritchi@ualberta.ca>
2024-02-18 20:09:45 -0700

    Added a DeviceIDManager class to generate a unique device ID and store/read it in device storage

---
+++ app/src/main/java/ca/cmput301t05/placeholder/database/DeviceIDManager.java
@@ -0,0 +1,44 @@
+package ca.cmput301t05.placeholder.database;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.UUID;
+
+public class DeviceIDManager {
+
+    private static final String SHARED_PREFS_FILE_NAME = "PlaceholderAppPrefs";
+    private static final String DEV_ID_KEY = "deviceID";
+    private Context context;
+
+    public DeviceIDManager(Context context){
+        this.context = context;
+    }
+
+    public boolean deviceHasIDStored() {
+        SharedPreferences sharedPrefs = context.getSharedPreferences(SHARED_PREFS_FILE_NAME, Context.MODE_PRIVATE);
+        String deviceIDString = sharedPrefs.getString(DEV_ID_KEY, null);
+        return deviceIDString != null;
+    }
+
+    public UUID getDeviceID() {
+        SharedPreferences sharedPrefs = context.getSharedPreferences(SHARED_PREFS_FILE_NAME, Context.MODE_PRIVATE);
+
+        // Try to get the device ID from the preferences file
+        String deviceIDString = sharedPrefs.getString(DEV_ID_KEY, null);
+        // Check if the device ID exists
+        if(deviceIDString == null){
+            // Generate a new ID since there is no pre-existing one
+            UUID newID = UUID.randomUUID();
+            deviceIDString = newID.toString();
+
+            // Store the new ID in the preferences file
+            SharedPreferences.Editor prefsEditor = sharedPrefs.edit();
+            prefsEditor.putString(DEV_ID_KEY, deviceIDString);
+            prefsEditor.apply();
+        }
+
+        // Return the device ID
+        return UUID.fromString(deviceIDString);
+    }
+}
