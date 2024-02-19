beed577b7ba01198dbe791298ae68bf1c7c430a8
camgotbitten <jubinvillecam@gmail.com>
2024-02-18 19:41:48 -0700

    adding basic func

---
+++ app/src/main/java/ca/cmput301t05/placeholder/database/DatabaseManager.java
@@ -0,0 +1,15 @@
+package ca.cmput301t05.placeholder.database;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class DatabaseManager {
+
+    //store db
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public DatabaseManager(){
+
+    }
+
+
+}
