66272df9b59d0a683235046ddb7b6ee97922a1c5
camgotbitten <jubinvillecam@gmail.com>
2024-02-18 22:06:54 -0700

    Finished profile + notificaiton

--- app/src/main/java/ca/cmput301t05/placeholder/profile/Profile.java
+++ app/src/main/java/ca/cmput301t05/placeholder/profile/Profile.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import ca.cmput301t05.placeholder.events.Event;
+import ca.cmput301t05.placeholder.notifications.UserNotification;
 
 public class Profile {
 
@@ -16,11 +17,117 @@ public class Profile {
 
     List<Event> joinedEvents;
 
+    List<UserNotification> notifications;
+
     boolean isAdmin = false;
 
+    public Profile(String name){
+
+        this.name = name;
+
+        this.profileID = UUID.randomUUID();
 
 
+    }
+
+    public void joinEvent(Event event){
+
+        joinedEvents.add(event);
+
+    }
+
+    public void leaveEvent(Event event){
+
+        joinedEvents.remove(event);
+    }
+
+    public void hostEvent(Event e){
+        hostedEvents.add(e);
+    }
+
+    public void unHostEvent(Event e){
+        hostedEvents.remove(e);
+    }
+
+    public void addNotification(UserNotification a){
+
+        notifications.add(a);
+    }
+
+    public void removeNotification(UserNotification a){
+        notifications.remove(a);
+    }
+
+    //getters / setters
 
 
+    public List<Event> getHostedEvents() {
+        return hostedEvents;
+    }
 
+    public List<Event> getJoinedEvents() {
+        return joinedEvents;
+    }
+
+    public List<UserNotification> getNotifications() {
+        return notifications;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProfilePictureID() {
+        return profilePictureID;
+    }
+
+    public UUID getProfileID() {
+        return profileID;
+    }
+
+    public void setHomePage(String homePage) {
+        this.homePage = homePage;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public void setHostedEvents(List<Event> hostedEvents) {
+        this.hostedEvents = hostedEvents;
+    }
+
+    public void setJoinedEvents(List<Event> joinedEvents) {
+        this.joinedEvents = joinedEvents;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNotifications(List<UserNotification> notifications) {
+        this.notifications = notifications;
+    }
+
+    public void setProfileID(UUID profileID) {
+        this.profileID = profileID;
+    }
+
+    public void setProfilePictureID(String profilePictureID) {
+        this.profilePictureID = profilePictureID;
+    }
+
+    
 }
