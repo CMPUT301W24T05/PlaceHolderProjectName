e6ae6128abbb1ab25cdebf7ac7916c2e44e789e7
camgotbitten <jubinvillecam@gmail.com>
2024-02-18 21:38:51 -0700

    default profile

---
+++ app/src/main/java/ca/cmput301t05/placeholder/profile/Profile.java
@@ -0,0 +1,15 @@
+package ca.cmput301t05.placeholder.profile;
+
+import java.util.UUID;
+
+public class Profile {
+
+    UUID profileID;
+    String name, homePage, contactInfo;
+
+    String profilePictureID; //this will point to the collection in firebase storage
+
+
+
+
+}
