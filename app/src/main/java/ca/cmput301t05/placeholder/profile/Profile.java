c217234ed952667d51d2c0d8d50de550a300b7bb
camgotbitten <jubinvillecam@gmail.com>
2024-02-18 21:58:46 -0700

    adding more list

--- app/src/main/java/ca/cmput301t05/placeholder/profile/Profile.java
+++ app/src/main/java/ca/cmput301t05/placeholder/profile/Profile.java
@@ -1,6 +1,9 @@
 package ca.cmput301t05.placeholder.profile;
 
+import java.util.List;
 import java.util.UUID;
+
+import ca.cmput301t05.placeholder.events.Event;
 
 public class Profile {
 
@@ -8,6 +11,14 @@ public class Profile {
     String name, homePage, contactInfo;
 
     String profilePictureID; //this will point to the collection in firebase storage
+
+    List<Event> hostedEvents;
+
+    List<Event> joinedEvents;
+
+    boolean isAdmin = false;
+
+
 
 
 
