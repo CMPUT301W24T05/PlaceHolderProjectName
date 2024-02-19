2899644fb1aed4699f66dca673baab984aee544a
Max <mwritchi@ualberta.ca>
2024-02-18 21:32:04 -0700

    Fixed the uploading of images.

--- app/src/main/java/ca/cmput301t05/placeholder/database/ImageTable.java
+++ app/src/main/java/ca/cmput301t05/placeholder/database/ImageTable.java
@@ -1,5 +1,6 @@
 package ca.cmput301t05.placeholder.database;
 
+import android.content.Context;
 import android.net.Uri;
 
 import androidx.annotation.NonNull;
@@ -11,26 +12,31 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class ImageTable extends  DatabaseManager{
 
     //extends database manager which holds everything
 
-    public ImageTable(){
+    private Context context;
 
-        this.UploadFile("D:\\cmput301GIT\\PlaceHolderProjectName\\app\\src\\main\\java\\ca\\cmput301t05\\placeholder\\database\\yeet yah.jpg");
+    public ImageTable(Context context){
+        this.context = context;
 
     }
 
     //https://firebase.google.com/docs/storage/android/upload-files#java_2
 
-    public void UploadFile(String FilePath){
+    public void uploadFile(Uri file){
 
-        StorageReference storageRef = storage.getReference();
-        Uri file = Uri.fromFile(new File(FilePath));
+        StorageReference rootStorageRef = storage.getReference();
+
+        String filename = "images/" + UUID.randomUUID().toString() + ".jpg";
+        StorageReference storageRef = rootStorageRef.child(filename);
 
         StorageMetadata metadata = new StorageMetadata.Builder()
                 .setContentType("image/jpg")
@@ -51,6 +57,35 @@ public class ImageTable extends  DatabaseManager{
             }
         });
 
+    }
+
+    public void uploadResource(int resourceId){
+        Uri tempFileUri = copyResourceToTempFile(resourceId, "image_", ".jpg");
+        uploadFile(tempFileUri);
+    }
+
+    public Uri copyResourceToTempFile(int resourceId, String filePrefix, String fileSuffix) {
+        InputStream in = context.getResources().openRawResource(resourceId);
+        try {
+            // Create a temporary file in the app's cache directory
+            File tempFile = File.createTempFile(filePrefix, fileSuffix, context.getCacheDir());
+            OutputStream out = new FileOutputStream(tempFile);
+            byte[] buffer = new byte[1024];
+            int read;
+
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            out.flush();
+            out.close();
+
+            // Return a Uri to the temporary file
+            return Uri.fromFile(tempFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 
