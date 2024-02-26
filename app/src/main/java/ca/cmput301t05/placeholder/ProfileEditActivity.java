f2270fc0997e076b53d70d114835fe4be1b6735a
Galvin <js@gmail.com>
2024-02-25 23:11:37 -0700

    Update ProfileEditActivity.java
    Created setUp function to set up the profileEditing page with user's information.
    Create update function to update the user information when clicking on the save button, and then go back to the main page

--- app/src/main/java/ca/cmput301t05/placeholder/ProfileEditActivity.java
+++ app/src/main/java/ca/cmput301t05/placeholder/ProfileEditActivity.java
@@ -1,6 +1,11 @@
 package ca.cmput301t05.placeholder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +20,12 @@ public class ProfileEditActivity extends AppCompatActivity{
     private DeviceIDManager idManager;
     private UUID deviceID;
     private Profile profile;
+    private Button saveButton;
+    private Button adminButton;
+    private EditText editName;
+    private EditText editContact;
+    private ImageView profilePic;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +33,53 @@ public class ProfileEditActivity extends AppCompatActivity{
 
         idManager = new DeviceIDManager(this);
         deviceID = idManager.getDeviceID();
-        //profile =
+        // profile:
 
+        saveButton = findViewById(R.id.save_button);
+        adminButton = findViewById(R.id.admin_button);
+        editName = findViewById(R.id.edit_name);
+        editContact = findViewById(R.id.edit_contact);
+        profilePic = findViewById(R.id.profile_pic);
+
+        // First display the information store in the database,
+            // For example, getName will return "Name/Last Name" if no information get
+            // for example, getContactInfo will return "Contact Info" if no information get
+        setUp();
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                update();
+                Intent intent = new Intent(ProfileEditActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                update();
+                // will go to the admin page instead:
+                //Intent intent = new Intent(ProfileEditActivity.this, MainActivity.class);
+                //startActivity(intent);
+                //finish();
+            }
+        });
+    }
+
+    private void setUp(){
+        // Set image resource, converting byte code:
+        // profilePic.setImageResource(R.drawable.your_image_resource);
+        editName.setText(profile.getName());
+        editContact.setText(profile.getContactInfo());
+        boolean admin = profile.isAdmin();
+        if (admin == false){adminButton.setVisibility(View.INVISIBLE);}
+    }
+    private void update(){
+        profile.setName(editName.getText().toString());
+        profile.setContactInfo(editContact.getText().toString());
+        // Set image resource, converting byte code:
+        //profile.setProfilePictureID();
+        profile.updateDatabase();
+        // implement update dateBase function:
     }
 }
