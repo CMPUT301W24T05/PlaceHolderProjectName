7e533eb0c2fcc37768a4a5546baff8eaa488a3aa
toluLajide <tlajide@ualberta.ca>
2024-03-07 01:29:51 -0700

    Made The QR code generation page
    Still working on it tho

---
+++ app/src/main/java/ca/cmput301t05/placeholder/ui/events/generateQRCodesActivity.java
@@ -0,0 +1,48 @@
+package ca.cmput301t05.placeholder.ui.events;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import ca.cmput301t05.placeholder.R;
+import ca.cmput301t05.placeholder.events.Event;
+import ca.cmput301t05.placeholder.events.QRCode;
+import ca.cmput301t05.placeholder.events.QRCodeManager;
+
+public class generateQRCodesActivity extends AppCompatActivity {
+
+    Button existingQR_btn, generateQR_btn, back_btn;
+    QRCodeManager QRCM;
+
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_generateqr);
+
+        existingQR_btn = findViewById(R.id.scan_existing_qr);
+        generateQR_btn = findViewById(R.id.generate_new_qr);
+        back_btn = findViewById(R.id.back_button);
+        QRCM = new QRCodeManager();
+
+        //Here should I get the event from the previous intent as a Serializable object or should I pull it from the database
+        //String event = getIntent().getExtras().getString("event");
+
+        generateQR_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                QRCode qr = QRCM.generateQRCode(event);
+
+                // complete this
+                Intent i = new Intent(generateQRCodesActivity.this, qrCompleteFrag.class);
+                startActivity(i);
+
+
+            }
+        });
+    }
+}
--- app/src/test/java/ca/cmput301t05/placeholder/QRCodeTest.java
+++ app/src/test/java/ca/cmput301t05/placeholder/QRCodeTest.java
@@ -1,10 +1,12 @@
 package ca.cmput301t05.placeholder;
 
+
 import org.junit.Rule;
 import org.junit.Test;
 
 import ca.cmput301t05.placeholder.database.DatabaseManager;
 import ca.cmput301t05.placeholder.events.Event;
+import ca.cmput301t05.placeholder.events.QRCode;
 import ca.cmput301t05.placeholder.events.QRCodeManager;
 
 public class QRCodeTest {
@@ -15,7 +17,10 @@ public class QRCodeTest {
     @Test
     public void generatorTest(){
         Event event = new Event("exampleEvent", "this is my event info", 100);
-        QRCM.generateQRCode(event, "eventinfo");
+        QRCode infoqr = QRCM.generateQRCode(event, "eventinfo");
+
+
+
 
     }
 }
