2bee4139e1d0e9da2ab802d054da13d86f1a5352
jubinvillecameron <jubinvillecam@gmail.com>
2024-03-06 19:11:37 -0700

    Adding the buttons and such

--- app/src/main/java/ca/cmput301t05/placeholder/ui/events/ViewEventDetailsActivity.java
+++ app/src/main/java/ca/cmput301t05/placeholder/ui/events/ViewEventDetailsActivity.java
@@ -1,6 +1,8 @@
 package ca.cmput301t05.placeholder.ui.events;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +10,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import ca.cmput301t05.placeholder.R;
 
 public class ViewEventDetailsActivity extends AppCompatActivity {
+
+    private Button Back;
+
+    private Button profileButton;
+
+    private TextView event_date;
+
+    private TextView event_Location;
+
+    private TextView event_details;
+
+    private TextView event_author;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
