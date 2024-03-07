037f0fd80852a30eba55d114f0e8555c6c881af1
jubinvillecameron <jubinvillecam@gmail.com>
2024-03-06 18:12:10 -0700

    making the event details page

---
+++ app/src/main/java/ca/cmput301t05/placeholder/ui/events/ViewEventDetailsActivity.java
@@ -0,0 +1,20 @@
+package ca.cmput301t05.placeholder.ui.events;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import ca.cmput301t05.placeholder.R;
+
+public class ViewEventDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //assume we grab the event id from a bundle and use that to display
+        setContentView(R.layout.event_vieweventdetails);
+
+    }
+}
