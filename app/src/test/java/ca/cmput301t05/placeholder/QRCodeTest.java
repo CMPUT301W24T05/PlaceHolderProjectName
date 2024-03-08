feb9d2ac54fe7fb78fe6406958464d5c6f9793f5
Max <mwritchi@ualberta.ca>
2024-03-07 20:10:31 -0700

    Commenting out incomplete QR code tests

--- app/src/test/java/ca/cmput301t05/placeholder/QRCodeTest.java
+++ app/src/test/java/ca/cmput301t05/placeholder/QRCodeTest.java
@@ -11,16 +11,16 @@ import ca.cmput301t05.placeholder.events.QRCodeManager;
 
 public class QRCodeTest {
 
-    @Rule
-    QRCodeManager QRCM = new QRCodeManager();
-
-    @Test
-    public void generatorTest(){
-        Event event = new Event("exampleEvent", "this is my event info", 100);
-        QRCode infoqr = QRCM.generateQRCode(event, "eventinfo");
-
-
-
-
-    }
+//    @Rule
+//    QRCodeManager QRCM = new QRCodeManager();
+//
+//    @Test
+//    public void generatorTest(){
+//        Event event = new Event("exampleEvent", "this is my event info", 100);
+//        QRCode infoqr = QRCM.generateQRCode(event, "eventinfo");
+//
+//
+//
+//
+//    }
 }
