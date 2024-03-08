01629fe9294dff9910473360d5632715f3b74185
camgotbitten <jubinvillecam@gmail.com>
2024-03-07 20:10:40 -0700

    adding more documentation

--- app/src/main/java/ca/cmput301t05/placeholder/events/QRCode.java
+++ app/src/main/java/ca/cmput301t05/placeholder/events/QRCode.java
@@ -1,8 +1,6 @@
 package ca.cmput301t05.placeholder.events;
 
 import android.graphics.Bitmap;
-import android.media.Image;
-import android.widget.ImageView;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
@@ -13,12 +11,23 @@ import com.journeyapps.barcodescanner.BarcodeEncoder;
 import java.util.Objects;
 import java.util.UUID;
 
+/**
+ * Represents a QR Code for an event, encapsulating the event ID and the type of QR code.
+ * The QR code can be of two types: infoQR (true) for event information or checkInQR (false) for event check-in.
+ * This class utilizes the ZXing ("Zebra Crossing") library for encoding the event's ID and type into a QR code.
+ */
 public class QRCode {
     UUID eventID;
     public Boolean type; // true if infoQR, false if checkInQR
 
 
-    // constructs a QRCode object and encodes it with the eventID of the event that it's for
+    /**
+     * Constructs a QRCode object and encodes it with the eventID of the event that it's for,
+     * along with the specified type ("eventinfo" for an info QR code, otherwise a check-in QR code).
+     *
+     * @param event The event for which the QR code is generated.
+     * @param type The type of QR code: "eventinfo" for an info QR code, any other value for a check-in QR code.
+     */
     public QRCode(Event event, String type){
         if (Objects.equals(type, "eventinfo")) {this.type = true;}
         else {this.type = false;}
@@ -42,9 +51,18 @@ public class QRCode {
     }
 
 
-
+    /**
+     * Gets the type of the QR code.
+     *
+     * @return Boolean indicating the type of QR code: true for infoQR, false for checkInQR.
+     */
     public Boolean getType(){return this.type;}
 
+    /**
+     * Sets the type of the QR code.
+     *
+     * @param type Boolean indicating the type of QR code: true for infoQR, false for checkInQR.
+     */
     public void setType(Boolean type){this.type = type;}
 
 }
