395b03bc1609ea95de722642a1a86022f3889c85
Galvinn <liuhaiwen26@hotmail.com>
2024-03-17 22:24:34 -0600

    Location of the user is successfully returned!

--- app/src/main/java/ca/cmput301t05/placeholder/Location/LocationManager.java
+++ app/src/main/java/ca/cmput301t05/placeholder/Location/LocationManager.java
@@ -5,37 +5,121 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
+import android.util.Log;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class LocationManager {
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1001;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private Context context;
+    private LocationPermissionListener permissionListener;
+
+    // LocationPermissionListener is implemented by the Successful_Checked_In_Activity class
+    // so that I can call the onRequestPermissionsResult in the activity and pass the result back
+    // to the LocationManager
+    public interface LocationPermissionListener {
+        void onLocationPermissionGranted();
+        void onLocationPermissionDenied();
+    }
+    public void setLocationPermissionListener(LocationPermissionListener listener) {
+        permissionListener = listener;
+    }
 
     public LocationManager(Context context){
         this.context = context;
         this.fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
     }
 
-    private boolean checkLocationPermission() {
+    public boolean checkLocationPermission() {
         // Check if the ACCESS_FINE_LOCATION permission has been granted
         return ContextCompat.checkSelfPermission(context,
                 Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
 
     // Activity is the screen that you want the pop up window to appear
-    private void requestLocationPermission(Activity activity) {
+    public void requestLocationPermission(Activity activity) {
         // Request the ACCESS_FINE_LOCATION permission from the user
         ActivityCompat.requestPermissions(activity,
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                 LOCATION_PERMISSION_REQUEST_CODE);
     }
 
+    // Delegate the taks to the activity class by using the permissionListen interface
+    public void onRequestPermissionsResult(int requestCode, int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (permissionListener != null) {
+                    permissionListener.onLocationPermissionGranted();
+                }
+            } else {
+                if (permissionListener != null) {
+                    permissionListener.onLocationPermissionDenied();
+                }
+            }
+        }
+    }
+
+//    public Location getLastLocation() {
+//        final Location[] lastLocation = {null};
+//        if (checkLocationPermission()) {
+//            fusedLocationProviderClient.getLastLocation()
+//                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+//                        @Override
+//                        public void onSuccess(Location location) {
+//                            if (location != null) {
+//                                lastLocation[0] = location;
+                                    // skip it because of
+//                            }
+//
+//                        }
+//                    });
+//        } else {
+//            permissionListener.onLocationPermissionDenied();
+//        }
+//        Log.d("Location", "lastLocation[0] = " + lastLocation[0]);
+//        return lastLocation[0];
+//    }
+
+    public interface LocationCallback {
+        void onLocationReceived(Location location);
+    }
+
+    // the location returned can be Null (not granted access), since location might not be available immediately
+    // Instead of directly returning the last location from the method,
+    // you should use a callback mechanism or a listener to handle the location when it becomes available.
+    // the location is a location object if (successfully get the location)
+    public void getLastLocation(final LocationCallback callback) {
+        if (checkLocationPermission()) {
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                callback.onLocationReceived(location);
+                                // onSuccess will takes some time, and may not happen instantly
+                                // have the callback interface, where we implement the method in the activity.class
+                                // so all the code related to the location will only be executed when it is onSuccess
+                                Log.d("Location", "lastLocation is not null!");
+                            } else {
+                                // If location is null, handle the case accordingly
+                                Log.d("Location", "lastLocation is null!");
+                            }
+                        }
+                    });
+        } else {
+            permissionListener.onLocationPermissionDenied();
+        }
+    }
+
+    // return the location
 
 
 }
--- app/src/main/java/ca/cmput301t05/placeholder/Location/Successful_Checked_In_Activity.java
+++ app/src/main/java/ca/cmput301t05/placeholder/Location/Successful_Checked_In_Activity.java
@@ -1,15 +1,17 @@
 package ca.cmput301t05.placeholder.Location;
 
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import ca.cmput301t05.placeholder.PlaceholderApp;
 import ca.cmput301t05.placeholder.R;
 
-public class Successful_Checked_In_Activity extends AppCompatActivity {
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1001;
+public class Successful_Checked_In_Activity extends AppCompatActivity implements LocationManager.LocationPermissionListener {
     private LocationManager locationManager;
     private PlaceholderApp app;
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +20,35 @@ public class Successful_Checked_In_Activity extends AppCompatActivity {
         setContentView(R.layout.checked_in_page);
 
         locationManager = app.getLocationManager();
+        locationManager.setLocationPermissionListener(this);
+        // Request Location Permission by showing the pop up windows, this will automatically call OnRequestPermissionsResult()
+        locationManager.requestLocationPermission(this);
     }
-    public void onRequestPermissionsResult(int requestCode, String[] permissions,int[] grantResults) {
+
+    // Overriden method from the activity class, will call the onRequestPermissionsResult in LocationManagerClass
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Location permission granted, proceed with accessing location services
-                accessLocationServices();
-            } else {
-                // Location permission denied by the user
-                // Handle this case, e.g., display a message or disable location features
-            }
-        }
+        // Forward the permission request results to the LocationManager
+        locationManager.onRequestPermissionsResult(requestCode, grantResults);
     }
+
+    public void onLocationPermissionGranted(){
+        Toast.makeText(this, "onLocationPermissionGranted", Toast.LENGTH_SHORT).show();
+        locationManager.getLastLocation(new LocationManager.LocationCallback() {
+            @Override
+            public void onLocationReceived(Location location) {
+                if (location!=null){
+                    // testing to see my location on screen
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    String message = "Latitude: "+latitude + " Longtitude: " + longitude;
+                    Toast.makeText(Successful_Checked_In_Activity.this, message, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    };
+    public void onLocationPermissionDenied(){
+        Toast.makeText(this, "onLocationPermissionDenied", Toast.LENGTH_SHORT).show();
+    };
 }
