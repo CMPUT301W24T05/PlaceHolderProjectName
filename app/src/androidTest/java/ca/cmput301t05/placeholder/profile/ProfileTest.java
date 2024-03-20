9f1a1fa80236b30ae665e9499e9444d90cef4047
Anthony <132036962+Anthony-Mirza@users.noreply.github.com>
2024-03-20 17:39:04 -0600

    update

--- app/src/androidTest/java/ca/cmput301t05/placeholder/profile/ProfileTest.java
+++ app/src/androidTest/java/ca/cmput301t05/placeholder/profile/ProfileTest.java
@@ -1,7 +1,9 @@
 package ca.cmput301t05.placeholder.profile;
+import android.os.SystemClock;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -59,9 +61,10 @@ public class ProfileTest {
         public void testProfilePageCorrectName(){ // Checks that when user's name matches on profile page
             // Will launch initial page where user prompted to enter name
             onView(withId(R.id.intro_name_edit)).perform(click());
-            onView(withId(R.id.intro_name_edit)).perform(ViewActions.typeText("Dave")); // Entering name
+            onView(withId(R.id.intro_name_edit)).perform(ViewActions.typeText("Dave"), closeSoftKeyboard()); // Entering name
             // Will go to the main activity homepage
             onView(withId(R.id.intro_submit_button)).perform(click());
+            SystemClock.sleep(5000);
             onView(withId(R.id.btnProfile)).perform(click()); // Click on profile button
             onView(ViewMatchers.withId(R.id.edit_name))
                     // Check that the text matches the expected name "Dave"
