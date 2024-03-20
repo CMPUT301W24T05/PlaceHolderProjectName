91d15b0af2d9129f1b6a85c79351f3e0c6735411
Anthony <132036962+Anthony-Mirza@users.noreply.github.com>
2024-03-20 16:56:19 -0600

    commit

--- app/src/androidTest/java/ca/cmput301t05/placeholder/profile/ProfileTest.java
+++ app/src/androidTest/java/ca/cmput301t05/placeholder/profile/ProfileTest.java
@@ -1,7 +1,75 @@
 package ca.cmput301t05.placeholder.profile;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.jupiter.api.Nested;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.UUID;
+
+import ca.cmput301t05.placeholder.InitialSetupActivity;
+import ca.cmput301t05.placeholder.MainActivity;
+import ca.cmput301t05.placeholder.R;
+import ca.cmput301t05.placeholder.events.Event;
+
 public class ProfileTest {
+
+    public Profile mockProfile(){
+        return new Profile("Bob", UUID.randomUUID());
+    }
+
+    public Event mockEvent(){
+        return new Event("Soccer Game", "Bring water bottle", 40);
+    }
+    @Test
+    public void testJoinEvent() {
+        // Tests joining and leaving event functionality in profile
+        Profile userProfile = mockProfile();
+        Event event  = mockEvent();
+        userProfile.joinEvent(event);
+        assertEquals(userProfile.getJoinedEvents().size(), 1); // Joined one event should should return one event
+        userProfile.leaveEvent(event);
+        assertEquals(userProfile.getJoinedEvents().size(), 0);
+    }
+
+
+    @Nested
+    @RunWith(MockitoJUnitRunner.class)
+    public static class intentTestProfilePage {
+        // Intent test. Different from unit test above.
+
+        @Rule
+        // Launch the actual activity
+        public ActivityScenarioRule<InitialSetupActivity> scenario = new
+                ActivityScenarioRule<InitialSetupActivity>(InitialSetupActivity.class);
+
+        @Test
+        public void testProfilePageCorrectName(){ // Checks that when user's name matches on profile page
+            // Will launch initial page where user prompted to enter name
+            onView(withId(R.id.intro_name_edit)).perform(click());
+            onView(withId(R.id.intro_name_edit)).perform(ViewActions.typeText("Dave")); // Entering name
+            // Will go to the main activity homepage
+            onView(withId(R.id.intro_submit_button)).perform(click());
+            onView(withId(R.id.btnProfile)).perform(click()); // Click on profile button
+            onView(ViewMatchers.withId(R.id.edit_name))
+                    // Check that the text matches the expected name "Dave"
+                    .check(matches(withText("Dave")));
+        }
+
+
+
+    }
 
 }
