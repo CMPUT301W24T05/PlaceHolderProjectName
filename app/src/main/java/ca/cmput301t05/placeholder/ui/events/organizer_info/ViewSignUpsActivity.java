b5b18a42bc6311915c7ec100d750e360c550ca75
Anthony <132036962+Anthony-Mirza@users.noreply.github.com>
2024-03-31 19:37:49 -0600

    Fixed updating checkin and signup list in real time

--- app/src/main/java/ca/cmput301t05/placeholder/ui/events/organizer_info/ViewSignUpsActivity.java
+++ app/src/main/java/ca/cmput301t05/placeholder/ui/events/organizer_info/ViewSignUpsActivity.java
@@ -13,10 +13,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import ca.cmput301t05.placeholder.PlaceholderApp;
 import ca.cmput301t05.placeholder.R;
+import ca.cmput301t05.placeholder.database.tables.EventTable;
 import ca.cmput301t05.placeholder.database.tables.ProfileTable;
 import ca.cmput301t05.placeholder.database.tables.Table;
 import ca.cmput301t05.placeholder.events.Event;
@@ -32,10 +34,14 @@ public class ViewSignUpsActivity extends AppCompatActivity {
 
     private ProfileTable profileTable;
 
+    private EventTable eventTable;
+
     private ArrayList<String> signupNames;
 
     private SignUpViewAdapter signUpViewAdapter;
     private Toolbar toolbar;
+
+
 
     private TextView emptyTextView;
 
@@ -45,12 +51,11 @@ public class ViewSignUpsActivity extends AppCompatActivity {
         app = (PlaceholderApp) getApplicationContext();
         setContentView(R.layout.view_signups_activity);
         event = app.getCachedEvent();
-        signUps = event.getRegisteredUsers();
-        signupNames = new ArrayList<String>();
-        Log.e("amirza2", String.valueOf(event.getRegisteredUsers()));
+        UUID eventID = event.getEventID();
 
-        int numOfSignUps  = signUps.size();
         profileTable = app.getProfileTable();
+        eventTable = app.getEventTable();
+
         toolbar = findViewById(R.id.toolbarSignups);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,54 +64,63 @@ public class ViewSignUpsActivity extends AppCompatActivity {
             }
         });
 
-        signUpRecyclerView  = findViewById(R.id.signups_recyclerview);
-        emptyTextView = findViewById(R.id.emptyTextView4Signup);
-        Log.e("amirza2", String.valueOf(numOfSignUps));
 
-        if (numOfSignUps <= 0){
-            signUpRecyclerView.setVisibility(View.GONE);
-            emptyTextView.setVisibility(View.VISIBLE);
-            Toast.makeText(ViewSignUpsActivity.this, "You have no signups!", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            final AtomicInteger counter = new AtomicInteger(0); // Counter to track successful fetches
-            for (int i = 0; i < numOfSignUps; i++) {
-                String uuid = signUps.get(i);
-                profileTable.fetchDocument(uuid, new Table.DocumentCallback<Profile>() {
-                    @Override
-                    public void onSuccess(Profile document) {
-                        signupNames.add(document.getName()); // Add the profile's name to the array list to display
-                        if (counter.incrementAndGet() == numOfSignUps) { // Check if all documents have been fetched
-                            signUpViewAdapter = new SignUpViewAdapter(ViewSignUpsActivity.this, signupNames);
-                            signUpRecyclerView.setVisibility(View.VISIBLE);
-                            emptyTextView.setVisibility(View.GONE);
-                            signUpRecyclerView.setAdapter(signUpViewAdapter);
-                            signUpRecyclerView.setLayoutManager(new LinearLayoutManager(ViewSignUpsActivity.this));
-                        }
-                    }
+        eventTable.fetchDocument(String.valueOf(eventID), new Table.DocumentCallback<Event>() {
+            @Override
+            public void onSuccess(Event document) {
 
-                    @Override
-                    public void onFailure(Exception e) {
-                        Log.e("amirza2", "Profile table access failed inside ViewSignUpsActivity");
-                        if (counter.incrementAndGet() == numOfSignUps) { // Check if all documents have been fetched
-                            // If there's a failure, still try to set up the adapter
-                            signUpViewAdapter = new SignUpViewAdapter(ViewSignUpsActivity.this, signupNames);
-                            signUpRecyclerView.setVisibility(View.VISIBLE);
-                            emptyTextView.setVisibility(View.GONE);
-                            signUpRecyclerView.setAdapter(signUpViewAdapter);
-                            signUpRecyclerView.setLayoutManager(new LinearLayoutManager(ViewSignUpsActivity.this));
-                        }
+                signUps = document.getRegisteredUsers();
+                signupNames = new ArrayList<String>();
+                int numOfSignUps  = signUps.size();
+                signUpRecyclerView  = findViewById(R.id.signups_recyclerview);
+                emptyTextView = findViewById(R.id.emptyTextView4Signup);
+                Log.e("amirza2", String.valueOf(numOfSignUps));
+
+                if (numOfSignUps <= 0){
+                    signUpRecyclerView.setVisibility(View.GONE);
+                    emptyTextView.setVisibility(View.VISIBLE);
+                    Toast.makeText(ViewSignUpsActivity.this, "You have no signups!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    final AtomicInteger counter = new AtomicInteger(0); // Counter to track successful fetches
+                    for (int i = 0; i < numOfSignUps; i++) {
+                        String uuid = signUps.get(i);
+                        profileTable.fetchDocument(uuid, new Table.DocumentCallback<Profile>() {
+                            @Override
+                            public void onSuccess(Profile document) {
+                                signupNames.add(document.getName()); // Add the profile's name to the array list to display
+                                if (counter.incrementAndGet() == numOfSignUps) { // Check if all documents have been fetched
+                                    signUpViewAdapter = new SignUpViewAdapter(ViewSignUpsActivity.this, signupNames);
+                                    signUpRecyclerView.setVisibility(View.VISIBLE);
+                                    emptyTextView.setVisibility(View.GONE);
+                                    signUpRecyclerView.setAdapter(signUpViewAdapter);
+                                    signUpRecyclerView.setLayoutManager(new LinearLayoutManager(ViewSignUpsActivity.this));
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Exception e) {
+                                Log.e("amirza2", "Profile table access failed inside ViewSignUpsActivity");
+                                if (counter.incrementAndGet() == numOfSignUps) { // Check if all documents have been fetched
+                                    // If there's a failure, still try to set up the adapter
+                                    signUpViewAdapter = new SignUpViewAdapter(ViewSignUpsActivity.this, signupNames);
+                                    signUpRecyclerView.setVisibility(View.VISIBLE);
+                                    emptyTextView.setVisibility(View.GONE);
+                                    signUpRecyclerView.setAdapter(signUpViewAdapter);
+                                    signUpRecyclerView.setLayoutManager(new LinearLayoutManager(ViewSignUpsActivity.this));
+                                }
+                            }
+                        });
                     }
-                });
+                }
+
             }
-        }
-//            signUpViewAdapter = new SignUpViewAdapter(ViewSignUpsActivity.this, signupNames);
-//            signUpRecyclerView.setVisibility(View.VISIBLE);
-//            emptyTextView.setVisibility(View.GONE);
-//            signUpRecyclerView.setAdapter(signUpViewAdapter);
-//            signUpRecyclerView.setLayoutManager(new LinearLayoutManager(ViewSignUpsActivity.this));
 
-
+            @Override
+            public void onFailure(Exception e) {
+                Log.e("amirza2", "Failed in ViewSignUpsActivity");
+            }
+        });
 
     }
 }
