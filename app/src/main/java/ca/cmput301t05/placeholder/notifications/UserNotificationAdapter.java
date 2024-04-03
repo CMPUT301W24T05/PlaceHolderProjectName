0f1ce053fb97257239c84149dbb5514847de8463
jubinvillecameron <jubinvillecam@gmail.com>
2024-04-03 15:43:55 -0600

    User Notification adapter is finished

--- app/src/main/java/ca/cmput301t05/placeholder/notifications/UserNotificationAdapter.java
+++ app/src/main/java/ca/cmput301t05/placeholder/notifications/UserNotificationAdapter.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +20,8 @@ import ca.cmput301t05.placeholder.PlaceholderApp;
 import ca.cmput301t05.placeholder.R;
 import ca.cmput301t05.placeholder.database.tables.Table;
 import ca.cmput301t05.placeholder.events.Event;
+import ca.cmput301t05.placeholder.utils.DateStrings;
+import ca.cmput301t05.placeholder.utils.StringManip;
 import ca.cmput301t05.placeholder.utils.holdNotiEvent;
 
 public class UserNotificationAdapter extends RecyclerView.Adapter<UserNotificationAdapter.UserNotificationHolder> {
@@ -100,11 +105,73 @@ public class UserNotificationAdapter extends RecyclerView.Adapter<UserNotificati
 
     public class UserNotificationHolder extends RecyclerView.ViewHolder{
 
+        TextView eventName, notificationMessage, notificationTime;
+        CardView card;
+
         public UserNotificationHolder(@NonNull View itemView) {
             super(itemView);
+
+            eventName = itemView.findViewById(R.id.user_notification_card_eventName);
+            notificationMessage = itemView.findViewById(R.id.user_notification_card_message);
+            notificationTime = itemView.findViewById(R.id.user_notification_card_notifDate);
+            card = itemView.findViewById(R.id.user_notification_card);
+
+            View.OnClickListener expandCollapseListener = v -> {
+
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION){
+                    boolean isExpanded = itemExpanded.getOrDefault(position, false);
+                    itemExpanded.put(position, !isExpanded);
+                    notiEvents.get(position).getN().setRead(true);
+                    card.setCardBackgroundColor(context.getColor(R.color.md_theme_background_highContrast));
+
+                    notifyItemChanged(position);
+                }
+
+            };
+
+            card.setOnClickListener(expandCollapseListener);
+
+
         }
 
         public void bindView(int position){
+
+            Notification n = notiEvents.get(position).getN();
+            assert n != null;
+
+            if (!n.isRead()){
+
+                //set card background to different colour
+                card.setCardBackgroundColor(context.getColor(R.color.md_theme_inversePrimary));
+
+            }
+
+            eventName.setText(notiEvents.get(position).getE().getEventName());
+
+            if (!itemExpanded.getOrDefault(position, false)){
+                //truncate message
+                notificationMessage.setText(StringManip.truncateString(n.getMessage(), 50));
+            }   else {
+                notificationMessage.setText(n.getMessage());
+            }
+
+            Calendar c = n.getTimeCreated();
+
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            int hour = c.get(Calendar.HOUR);
+            int amPM = c.get(Calendar.AM_PM);
+
+            String monthName = DateStrings.getMonthName(month);
+            String dayString = String.valueOf(day);
+
+            String amOrPmString = DateStrings.getAmPM(amPM);
+            String hourString = String.valueOf(hour);
+
+            String formatNotifTime = hourString + " " + amOrPmString + ",  " + monthName + " " + dayString;
+            notificationTime.setText(formatNotifTime);
 
         }
     }
