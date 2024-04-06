d25107409c0b8731ffd5b67c69573802d4980c92
Max <mwritchi@ualberta.ca>
2024-04-05 19:23:34 -0600

    Refactor EventAdapter and ViewHolder classes structure
    The EventAdapter and ViewHolder classes in EventAdapter.java have been streamlined and refactored for improved clarity and functionality. In bindView method, usage of SimpleDateFormat has been updated for efficiency and exception handling, error logging and validation checks are implemented for better robustness. Also, reorganized and divided logic into smaller methods facilitating readability and debugging processes.

--- app/src/main/java/ca/cmput301t05/placeholder/events/EventAdapter.java
+++ app/src/main/java/ca/cmput301t05/placeholder/events/EventAdapter.java
@@ -27,21 +27,19 @@ import ca.cmput301t05.placeholder.R;
 import ca.cmput301t05.placeholder.database.images.BaseImageHandler;
 
 public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventCardViewHolder> {
-
     private ArrayList<Event> eventList;
     private final Context context;
-
     public enum adapterType{
         HOSTED,
         ATTENDING
     }
-
     public interface OnItemClickListener{
         void onItemClick(Event event, adapterType type);
     }
-
     private adapterType type;
     private OnItemClickListener listener;
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MMMM dd, yyyy 'at' hh:mm a", Locale.CANADA);
 
 
     public EventAdapter(Context context, ArrayList<Event> event, adapterType adapterType){
@@ -73,7 +71,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventCardVie
     public void onBindViewHolder(@NonNull EventCardViewHolder holder, int position) {
 
         try {
-            holder.bindView(position, listener);
+            holder.bindView(position);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -91,172 +89,124 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventCardVie
     }
 
     public class EventCardViewHolder extends RecyclerView.ViewHolder {
-
-        TextView eventName, eventLocation, eventDate, eventTime;
+        TextView eventName, eventLocation, eventDate;
+        ShapeableImageView posterView;
+        PlaceholderApp app;
         CardView cardView;
 
         public EventCardViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            // Ensure the view ids exist
+            try{
+                eventName = itemView.findViewById(R.id.event_name);
+                eventLocation = itemView.findViewById(R.id.event_location);
+                eventDate = itemView.findViewById(R.id.event_date);
+                cardView = itemView.findViewById(R.id.event_card);
+                posterView = itemView.findViewById(R.id.poster_view);
 
-            eventName = itemView.findViewById(R.id.event_name);
-            eventLocation = itemView.findViewById(R.id.event_location);
-            eventDate = itemView.findViewById(R.id.event_date);
-            eventTime = itemView.findViewById(R.id.event_time);
+            }catch(NullPointerException e){
+                Log.e("ViewHolderInit", "Error loading view holder: " + e.getMessage());
+            }
 
-            cardView = itemView.findViewById(R.id.event_card);
-
+            app = (PlaceholderApp) context.getApplicationContext();
         }
 
+        public void bindView(int position){
+            if(position >= 0 && position < eventList.size()) {
+                Event event = eventList.get(position);
+                bindBasicViews(event);
+                handlePosterLoad(event);
+                handleOnClick(event);
+            }
+        }
 
-        public void bindView(int position, OnItemClickListener listener){
-            Event event = eventList.get(position);
-            assert event != null;
-            eventName.setText(event.getEventName());
-            eventLocation.setText(event.getLocation());
+        public void handleOnClick(Event event){
+            if(listener != null) {
+                itemView.setOnClickListener(view -> listener.onItemClick(event, type));
+            }
+        }
 
-            Calendar calendar = event.getEventDate();
+        public void bindBasicViews(Event event){
+            if(event != null) {
+                eventName.setText(event.getEventName());
+                eventLocation.setText(event.getLocation());
 
-            SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd, yyyy", Locale.CANADA);
-            String formattedDate = dateFormat.format(calendar.getTime());
-            eventDate.setText(formattedDate);
+                String dateTime = DATE_FORMAT.format(event.getEventDate().getTime());
+                eventDate.setText(dateTime);
+            }
+        }
 
-            SimpleDateFormat timeFormat = new SimpleDateFormat("h:mma", Locale.CANADA);
-            String formattedTime = timeFormat.format(calendar.getTime()).toLowerCase();
-            eventTime.setText(formattedTime);
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    listener.onItemClick(event, type);
+        public void handlePosterLoad(Event event) {
+            if(event != null) {
+                if (event.hasEventPosterBitmap()) {
+                    posterView.setImageBitmap(event.getEventPosterBitmap());
+                } else {
+                    app.getPosterImageHandler().getPosterPicture(event, context, new BaseImageHandler.ImageCallback() {
+                        @Override
+                        public void onImageLoaded(Bitmap bitmap) {
+                            posterView.setImageBitmap(bitmap);
+                        }
+                        @Override
+                        public void onError(Exception e) {
+                            Log.e("Event_card View", "Error loading image: " + e.getMessage());
+                        }
+                    });
                 }
-            });
+            }
         }
     }
 
-    // created a subclass of the cardViewHolder for hosted event to show some statistics
     public class HostedEventCardViewHolder extends EventCardViewHolder {
         TextView interested_number, attended_number;
-        ShapeableImageView posterView;
-        PlaceholderApp app;
         public HostedEventCardViewHolder(@NonNull View itemView) {
             super(itemView);
-            interested_number = itemView.findViewById(R.id.interested_number);
-            attended_number = itemView.findViewById(R.id.checked_Max_card);
-            posterView = itemView.findViewById(R.id.poster_view);
-            app = (PlaceholderApp) context.getApplicationContext();
+            try{
+                interested_number = itemView.findViewById(R.id.interested_number);
+                attended_number = itemView.findViewById(R.id.checked_Max_card);
+            }catch(NullPointerException e){
+                Log.e("ViewHolderInit", "Error loading view holder: " + e.getMessage());
+            }
         }
         @Override
-        public void bindView(int position, OnItemClickListener listener){
-            Event event = eventList.get(position);
-            assert event != null;
-            eventName.setText(event.getEventName());
-            eventLocation.setText(event.getLocation());
-
-            Calendar calendar = event.getEventDate();
-
-            SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd, yyyy", Locale.CANADA);
-            String formattedDate = dateFormat.format(calendar.getTime());
-            eventDate.setText(formattedDate);
-
-            SimpleDateFormat timeFormat = new SimpleDateFormat("h:mma", Locale.CANADA);
-            String formattedTime = timeFormat.format(calendar.getTime()).toLowerCase();
-            eventTime.setText(formattedTime);
-
-             int max = event.getMaxAttendees();
-             long interested = event.getRegisteredUsersNum();
-             long attended = event.getAttendeesNum();
-             interested_number.setText(String.valueOf(interested));
-             String attended_string =  attended + "/" + max;
-             attended_number.setText(attended_string);
-
-             // set Bit map need to use the call back function
-             //posterView.setImageBitmap(event.getEventPosterBitmap());
-            if (event.hasEventPosterBitmap()) {
-                posterView.setImageBitmap(event.getEventPosterBitmap());
-            } else {
-                app.getPosterImageHandler().getPosterPicture(event, context, new BaseImageHandler.ImageCallback() {
-                    @Override
-                    public void onImageLoaded(Bitmap bitmap) {
-                        posterView.setImageBitmap(bitmap);
-                    }
-
-                    @Override
-                    public void onError(Exception e) {
-                        Log.e("Hoster_Event_card View", "Error loading image: " + e.getMessage());
-                    }
-                });
-            }
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    listener.onItemClick(event, type);
+        public void bindView(int position){
+            super.bindView(position);
+            if(position >= 0 && position < eventList.size()) {
+                Event event = eventList.get(position);
+                if(event != null){
+                    long interested = event.getRegisteredUsersNum();
+                    long attended = event.getAttendeesNum();
+                    interested_number.setText(String.valueOf(interested));
+                    String attended_string =  attended + "/" + event.getMaxAttendees();
+                    attended_number.setText(attended_string);
                 }
-            });
+            }
         }
     }
+
     public class JoinedEventCardViewHolder extends EventCardViewHolder {
-        TextView interested_number, attended_number;
-        ShapeableImageView posterView;
+        TextView interested_number;
         ImageView attendedIcon;
-        PlaceholderApp app;
         public JoinedEventCardViewHolder(@NonNull View itemView) {
             super(itemView);
-            interested_number = itemView.findViewById(R.id.interested_number);
-            attended_number = itemView.findViewById(R.id.checked_Max_card);
-            posterView = itemView.findViewById(R.id.poster_view);
-            attendedIcon = itemView.findViewById(R.id.imageView4);
-            app = (PlaceholderApp) context.getApplicationContext();
-
+            try {
+                interested_number = itemView.findViewById(R.id.interested_number);
+                attendedIcon = itemView.findViewById(R.id.imageView4);
+            }catch(NullPointerException e) {
+                Log.e("ViewHolderInit", "Error loading view holder: " + e.getMessage());
+            }
         }
         @Override
-        public void bindView(int position, OnItemClickListener listener){
-            Event event = eventList.get(position);
-            assert event != null;
-            eventName.setText(event.getEventName());
-            eventLocation.setText(event.getLocation());
-
-            Calendar calendar = event.getEventDate();
-
-            SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd, yyyy", Locale.CANADA);
-            String formattedDate = dateFormat.format(calendar.getTime());
-            eventDate.setText(formattedDate);
-
-            SimpleDateFormat timeFormat = new SimpleDateFormat("h:mma", Locale.CANADA);
-            String formattedTime = timeFormat.format(calendar.getTime()).toLowerCase();
-            eventTime.setText(formattedTime);
-
-            long interested = event.getRegisteredUsersNum();
-            interested_number.setText(String.valueOf(interested));
-
-            attendedIcon.setVisibility((View.INVISIBLE));
-            attended_number.setVisibility(View.INVISIBLE);
-
-            // set Bit map need to use the call back function
-            //posterView.setImageBitmap(event.getEventPosterBitmap());
-            if (event.hasEventPosterBitmap()) {
-                posterView.setImageBitmap(event.getEventPosterBitmap());
-            } else {
-                app.getPosterImageHandler().getPosterPicture(event, context, new BaseImageHandler.ImageCallback() {
-                    @Override
-                    public void onImageLoaded(Bitmap bitmap) {
-                        posterView.setImageBitmap(bitmap);
-                    }
-
-                    @Override
-                    public void onError(Exception e) {
-                        Log.e("Hoster_Event_card View", "Error loading image: " + e.getMessage());
-                    }
-                });
-            }
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    listener.onItemClick(event, type);
+        public void bindView(int position){
+            super.bindView(position);
+            if(position >= 0 && position < eventList.size()) {
+                Event event = eventList.get(position);
+                if(event != null){
+                    long interested = event.getRegisteredUsersNum();
+                    interested_number.setText(String.valueOf(interested));
+                    attendedIcon.setVisibility(View.INVISIBLE);
                 }
-            });
+            }
         }
     }
 }
