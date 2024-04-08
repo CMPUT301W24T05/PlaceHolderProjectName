0f55847b1c80c34fec39ba69dfc78fbb3e103f68
Galvinn <117534018+Galvinn@users.noreply.github.com>
2024-04-08 02:49:52 -0600

    Update intentTestProfilePage.java

--- app/src/androidTest/java/ca/cmput301t05/placeholder/profile/intentTestProfilePage.java
+++ app/src/androidTest/java/ca/cmput301t05/placeholder/profile/intentTestProfilePage.java
@@ -10,7 +10,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
